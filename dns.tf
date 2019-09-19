fd970bdb1190b43a54e6ac15bffedfb6f01195b1
Jason Kennemer <jkennemer@Sarahs-MBP.lan>
2019-09-18 23:52:59 -0500

    dns updates

--- dns.tf
+++ dns.tf
@@ -7,6 +7,6 @@ resource "digitalocean_record" "do-droplet-dns-a" {
   count  = var.do_nodes
   domain = digitalocean_domain.default.name
   type   = "A"
-  name   = digitalocean_droplet.do_droplet.*.name
-  value  = digitalocean_droplet.do_droplet.*.ipv4_address
+  name   = "${element(digitalocean_droplet.do_droplet.*.name, count.index)}"
+  value  = "${element(digitalocean_droplet.do_droplet.*.ipv4_address, count.index)}"
 }
