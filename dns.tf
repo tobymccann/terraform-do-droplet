139f8a6291fb650181b0faab28c5ef9656c285d9
Jason Kennemer <jkennemer@Sarahs-MBP.lan>
2019-09-19 00:19:36 -0500

    domain name variable cleanup

--- dns.tf
+++ dns.tf
@@ -1,11 +1,6 @@
-# Create a new domain record
-resource "digitalocean_domain" "default" {
-  name       = var.domain_name
-}
-
 resource "digitalocean_record" "do-droplet-dns-a" {
   count  = var.do_nodes
-  domain = digitalocean_domain.default.name
+  domain = var.domain_name
   type   = "A"
   name   = "${element(digitalocean_droplet.do_droplet.*.name, count.index)}"
   value  = "${element(digitalocean_droplet.do_droplet.*.ipv4_address, count.index)}"
