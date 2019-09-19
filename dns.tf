50c6445d397951d2687233cc41d01fac212460b6
Jason Kennemer <jkennemer@Sarahs-MBP.lan>
2019-09-18 23:49:41 -0500

    dns updates

--- dns.tf
+++ dns.tf
@@ -1,8 +1,12 @@
-# 
+# Create a new domain record
+resource "digitalocean_domain" "default" {
+  name       = var.domain_name
+}
+
 resource "digitalocean_record" "do-droplet-dns-a" {
   count  = var.do_nodes
   domain = digitalocean_domain.default.name
   type   = "A"
-  name   = digitalocean_droplet.do_droplet.name
-  value  = digitalocean_droplet.do_droplet.ipv4_address
+  name   = digitalocean_droplet.do_droplet.*.name
+  value  = digitalocean_droplet.do_droplet.*.ipv4_address
 }
