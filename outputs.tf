686f9d1b547ffda52c457756f1c3be506d25d2a3
Jason Kennemer <jkennemer@Sarahs-MBP.lan>
2019-09-18 23:43:19 -0500

    adding index reference to account for count

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 output "do_droplet_public_ip" {
-  value = digitalocean_droplet.do_droplet.ipv4_address
+  value = digitalocean_droplet.do_droplet.*.ipv4_address
 }
 
 output "do_droplet_private_ip" {
-  value = digitalocean_droplet.do_droplet.ipv4_address_private
+  value = digitalocean_droplet.do_droplet.*.ipv4_address_private
 }
 
 output "do_droplet_id" {
-  value = digitalocean_droplet.do_droplet.id
+  value = digitalocean_droplet.do_droplet.*.id
 }
 
 output "do_droplet_urn" {
-  value = digitalocean_droplet.do_droplet.urn
+  value = digitalocean_droplet.do_droplet.*.urn
 }
